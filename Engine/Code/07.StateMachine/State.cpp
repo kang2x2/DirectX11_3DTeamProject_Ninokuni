eb512129e81220cdb90d348fad0a0b420e7c4d07
khkskange2 <kang2x2@naver.com>
2025-01-26 15:38:36 +0900

    2025-01-26
    담당 부분 정리

---
+++ Engine/Code/07.StateMachine/State.cpp
@@ -0,0 +1,64 @@
+#include "State.h"
+#include "GameObject.h"
+#include "StateMachine.h"
+#include "GameInstance.h"
+
+
+
+CState::CState(CStateMachine* pStateMachine)
+	: m_pStateMachineCom(pStateMachine)
+{	
+	
+}
+
+HRESULT CState::Initialize(const list<wstring>& AnimationList)
+{
+	m_pOwner = m_pStateMachineCom->Get_Owner();
+	if (nullptr == m_pOwner)
+		return E_FAIL;
+
+	m_pModelCom = m_pStateMachineCom->Get_Owner()->Get_Component_Model();
+	if (nullptr == m_pModelCom)
+		return E_FAIL;
+
+	m_pRigidBodyCom = m_pStateMachineCom->Get_Owner()->Get_Component_Rigidbody();
+	if (nullptr == m_pRigidBodyCom)
+		return E_FAIL;
+
+	m_pTransformCom = m_pStateMachineCom->Get_Owner()->Get_Component_Transform();
+	if (nullptr == m_pTransformCom)
+		return E_FAIL;
+
+
+
+	m_AnimIndices.reserve(AnimationList.size());
+	for (auto strAnimName : AnimationList)
+	{
+ 		_int iAnimIndex = m_pModelCom->Find_AnimationIndex(strAnimName);
+		if (-1 != iAnimIndex)
+			m_AnimIndices.push_back(iAnimIndex);
+		else
+		{
+			wstring strErrMsg = L"Find Animation Failed. : " + strAnimName + L"/ Object : " + m_pOwner->Get_PrototypeTag() + L"/ Model : " + m_pModelCom->Get_Name();
+			MessageBox(nullptr, strErrMsg.c_str(), L"System Message", MB_OK);
+			return E_FAIL;
+		}
+			
+	}
+
+	if (0 == m_AnimIndices.size())
+	{
+		MSG_BOX("Animation Size is 0");
+		return E_FAIL;
+	}
+		
+
+
+	return S_OK;
+}
+
+
+void CState::Free()
+{
+	__super::Free();
+}
